e28cc861a2a016a725f266edd959a93399f330b1
Енин Никита <happyniks28@gmail.com>
2021-07-12 18:49:48 +0500

    add `styles.css` and basic html template

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>	
+	<title></title>	
+	<link rel="shortcut icon" type="image/x-icon" href="assets/images/apple.ico">
+
+	<link href="styles.css" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Jura:700|Philosopher:400,700|Russo+One&display=swap&subset=cyrillic" rel="stylesheet">
+</head>
+<body>
+    <header class="header">
+        <div class="">
+        </div>
+        <div class="">
+        </div>
+    </header>
+</body>
+</html>
